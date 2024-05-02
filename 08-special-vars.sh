17e63380eca9ba2a831ead81de54abe5719f1461
Raja Leela Kumari <raja.leelakumari@siemens-healthineers.com>
2024-05-02 14:54:33 +0530

    learning avriables

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -3,3 +3,10 @@
 echo "All varibales: $@"
 echo  "Number of variables passed: $#"
 echo "script name:$0"
+echo "curremt working directory : $PWD"
+echo "curremt home directory : $HOME"
+echo "curremt user : $user"
+echo "hostname; $HOSTNAME"
+echo "process id of current command: $$"
+sleep 15
+echo "process id of last command : $!"
