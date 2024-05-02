286c300aaf212b1130d454a076875f5405f1af52
Raja Leela Kumari <raja.leelakumari@siemens-healthineers.com>
2024-05-02 12:08:26 +0530

    learning variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+Person1=Leela
+Person2=Ravi
+
+echo "$Person1 : Hi i am learning Devops"
+echo "$Person2 : Hi $Person1 , i am learing powerBI"
+echo $Person1: "Soon i will beocme devops engineer $Person2 "
+echo $Person2 : "Even i will be expert in powerbi"
