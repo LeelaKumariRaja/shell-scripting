f986d692749284dea74e02ffa7b42a05b710e960
Raja Leela Kumari <raja.leelakumari@siemens-healthineers.com>
2024-05-02 12:00:09 +0530

    learning variables

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+
+echo "Leela" : "Hi i am learning Devops"
+echo "Ravi" : "Hi Leela , i am learing powerBI"
+echo "Leela": "Soon i will beocme devops engineer Ravi "
+echo "Ravi" : "Even i will be expert in powerbi"
