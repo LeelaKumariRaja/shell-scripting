93f3a2dead4e8b7d271eeffff52173110e3c8f35
Raja Leela Kumari <raja.leelakumari@siemens-healthineers.com>
2024-05-02 12:19:08 +0530

    learning avriables

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
 #declare variable on top
 Person1=$1
 Person2=$2
-
 echo $Person1 : "Hi i am learning Devops"
 echo $Person2 : "Hi $Person1 , i am learing powerBI"
 echo $Person1: "Soon i will beocme devops engineer $Person2 "
