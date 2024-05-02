56db4f194146986f00630f79f220c326ca3f37c6
Raja Leela Kumari <raja.leelakumari@siemens-healthineers.com>
2024-05-02 13:17:47 +0530

    learning avriables

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -2,8 +2,8 @@
 
 Technologies=("TERRAFORM" "ANSIBLE" "PUPPET")
 
-echo "Main technology is $Technologies[0]"
+echo "Main technology is ${Technologies[0]}"
 
-echo "Next main is $Technologies[1]"
+echo "Next main is ${Technologies[1]}"
 
 echo "All technologies are ${Technologies[@]}"
