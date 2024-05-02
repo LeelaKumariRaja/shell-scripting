d81541e3dadb53a01b40536d90ce54a840ef684b
Raja Leela Kumari <raja.leelakumari@siemens-healthineers.com>
2024-05-02 18:12:08 +0530

    learning avriables

--- 13-intsallationArgumentPackage.sh
+++ 13-intsallationArgumentPackage.sh
@@ -31,7 +31,7 @@ for i in $@
     do
         echo "package to install: $i"
         dnf list installed $i &>> $LOGFILE
-        if[ $? -ne 0 ]
+        if [ $? -eq 0 ]
             then
                 echo "already $i installed.skipping"
         else
