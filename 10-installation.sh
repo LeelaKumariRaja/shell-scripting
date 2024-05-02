cb73733f95b5deda5b07c04e9b04411423bb7d60
Raja Leela Kumari <raja.leelakumari@siemens-healthineers.com>
2024-05-02 17:38:29 +0530

    learning avriables

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
 
 VALIDATE()
 {
@@ -21,10 +24,10 @@ else
     echo "You are super user"
 fi
 
-dnf install mysql -y
+dnf install mysqll -y &>> $LOGFILE
 
 VALIDATE  "Intsalling MYSQL"
 
-dnf install git -y
+dnf install git -y &>> $LOGFILE
 
 VALIDATE  "Intsalling GIT"
