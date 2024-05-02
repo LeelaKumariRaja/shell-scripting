de93c2d51bcda71780fc2032f779012a2fda16a8
Raja Leela Kumari <raja.leelakumari@siemens-healthineers.com>
2024-05-02 17:50:39 +0530

    learning avriables

---
+++ 12-colors.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE()
+{
+if [ $? -ne 0 ]
+then
+    echo " $1  $R failed"
+    exit 1
+else
+    echo "$1 is $G success. Going ahead"
+fi
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "execute with super user access"
+    exit 1
+else
+    echo "You are super user"
+fi
+
+dnf install mysql -y &>> $LOGFILE
+
+VALIDATE  "Intsalling MYSQL"
+
+dnf install git -y &>> $LOGFILE
+
+VALIDATE  "Intsalling GIT"
