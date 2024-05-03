c338918891ad490ed0b45caba5f4bafbfcd235ae
Raja Leela Kumari <raja.leelakumari@siemens-healthineers.com>
2024-05-03 15:31:08 +0530

    learning avriables

---
+++ 14-callingScript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+COURSE="from current script"
+
+echo "$COURSE"
+echo "PID is: $$"
+
+./15-otherScript.sh
+
+echo "$COURSE"
+echo "PID is: $$"
---
+++ 15-otherScript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+COURSE="from current script"
+
+echo "$COURSE"
+echo "PID is: $$"
