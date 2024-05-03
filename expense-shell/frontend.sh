e1187806f1c0c72fd36fc02b81c4733f05bcdf43
Raja Leela Kumari <raja.leelakumari@siemens-healthineers.com>
2024-05-03 12:30:35 +0530

    learning avriables

--- expense-shell/frontend.sh
+++ expense-shell/frontend.sh
@@ -39,16 +39,20 @@ VALIDATE "enabling nginx"
 systemctl start nginx &>> $LOGFILE
 VALIDATE "starting nginx"
 
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>> $LOGFILE
 VALIDATE "removing existing html content"
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>> $LOGFILE
 VALIDATE "getting frontentd code"
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip &>> $LOGFILE
 VALIDATE "going to path"
 
+cp /home/ec2-user/shell-scripting/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>> $LOGFILE
+VALIDATE "copying config folder"
 
+systemctl restart nginx &>> $LOGFILE
+VALIDATE "restarting nginx"
 
 
