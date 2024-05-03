f394697376d900a5ffb163c3dd2c69d3fb6b69a9
Raja Leela Kumari <raja.leelakumari@siemens-healthineers.com>
2024-05-03 06:45:41 +0530

    learning avriables

--- expense-shell/db.sh
+++ expense-shell/db.sh
@@ -2,6 +2,9 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 FILENAME=$(echo $0 | cut -d "/" -f2)
 LOGFILE=/tmp/$FILENAME-$TIMESTAMP.log
+echo "Please enter DB password"
+read -s mysql_root_pwd
+
 VALIDATE()
 {
     if [ $? -ne 0 ]
@@ -34,11 +37,22 @@ VALIDATE "starting  mysql-server"
 #mysql_secure_installation --set-root-pass ExpenseApp@1  &>>$LOGFILE
 #VALIDATE "setting password"
 
- mysql -h db.traindevops.online -uroot -pExpenseApp@1 -e 'SHOW DATABASES;' &>> $LOGFILE
+#  mysql -h db.traindevops.online -uroot -pExpenseApp@1 -e 'SHOW DATABASES;' &>> $LOGFILE
+
+#  if [ $? -ne 0 ]
+#  then 
+#     mysql_secure_installation --set-root-pass ExpenseApp@1  &>>$LOGFILE
+#     VALIDATE "setting root password"
+# else
+#     echo "mysql root password already set.skipping"
+# fi
+
+
+ mysql -h db.traindevops.online -uroot -p${mysql_root_pwd} -e 'SHOW DATABASES;' &>> $LOGFILE
 
  if [ $? -ne 0 ]
  then 
-    mysql_secure_installation --set-root-pass ExpenseApp@1  &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ${mysql_root_pwd}  &>>$LOGFILE
     VALIDATE "setting root password"
 else
     echo "mysql root password already set.skipping"
