175671cb5ced397894a4d377601000cb8ef49097
Raja Leela Kumari <raja.leelakumari@siemens-healthineers.com>
2024-05-10 17:04:36 +0530

    disk usage

--- 19-diskUsage.sh
+++ 19-diskUsage.sh
@@ -6,7 +6,7 @@ DISK_THRESHOLD=15
 while IFS= read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6f}' |cut -d "%" -f1)
-    FOLDER=$(echo $line | awk -F " " '{print $7f}')
+    FOLDER=$(echo $line | awk -F " " '{print $Nf}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
         echo $FOLDER is more than $DISK_THRESHOLD. current usage is $USAGE
